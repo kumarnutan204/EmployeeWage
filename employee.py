e1263a164c53f239b321db7badf6588a62753bf3
kumarnutan204 <kumarnutan204@gmail.com>
2024-02-12 17:02:26 +0530

    [Nutan] Added: Daily Employee wage calculation

--- employee.py
+++ employee.py
@@ -1,12 +1,24 @@
 import random 
 
-class Employee_Wage():
+class EmployeeWage:
+    def __init__(self) -> None:
+        self.wage_per_hour = 20
+        self.full_day = 8
+        self.total_wage = 0
+    
     def check_attendance(self):
-        self.attendance= random.choice(["present", "absent"])
-        if (self.attendance=='present'):
+        attendance = random.choice(["present", "absent"])
+        return attendance
+        
+        
+    def calculate_wage(self):
+        emp_status = self.check_attendance()
+        if emp_status == 'present':
+            self.total_wage = self.wage_per_hour * self.full_day
             print("the Employee is present")
         else:
             print("The Employee is absent")
-
-emp1= Employee_Wage()
-emp1.check_attendance()
+        
+emp1= EmployeeWage()
+emp1.calculate_wage()
+print(emp1.total_wage)
