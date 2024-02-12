ee911216dcf66a000fc6d3c345865ee558a973ee
kumarnutan204 <kumarnutan204@gmail.com>
2024-02-12 17:19:39 +0530

    [Nutan] Added: Part time Employee wage feature added

--- employee.py
+++ employee.py
@@ -1,13 +1,14 @@
 import random 
 
 class EmployeeWage:
-    def __init__(self) -> None:
+    def __init__(self):
         self.wage_per_hour = 20
         self.full_day = 8
         self.total_wage = 0
+        self.part_time_hr = 4
     
     def check_attendance(self):
-        attendance = random.choice(["present", "absent"])
+        attendance = random.choice(["present", "absent","half_day"])
         return attendance
         
         
@@ -16,9 +17,14 @@ class EmployeeWage:
         if emp_status == 'present':
             self.total_wage = self.wage_per_hour * self.full_day
             print("the Employee is present")
+        elif emp_status=="half_day":
+            self.total_wage = self.wage_per_hour * self.part_time_hr
+            print("the Employee is on Half day working")
+            
         else:
             print("The Employee is absent")
-        
-emp1= EmployeeWage()
-emp1.calculate_wage()
-print(emp1.total_wage)
+
+
+emp_part1= EmployeeWage()
+emp_part1.calculate_wage()
+print(emp_part1.total_wage)
