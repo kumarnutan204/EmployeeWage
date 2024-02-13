c68f083c71439ce3cf642894152b89b5b1b2ecc4
kumarnutan204 <kumarnutan204@gmail.com>
2024-02-13 12:08:54 +0530

    [Nutan] Added:UC8 - calculating the wage for different companies according to the parameters given by employee

--- employee.py
+++ employee.py
@@ -1,45 +1,55 @@
 import random 
 
 class EmployeeWage:
-    # def __init__(cls):
-    full_day = 8
-    wage_per_hour = 20
-    total_wage = 0
-    part_time_hr = 4
-    max_work_days=25
-    max_work_hours =100
-    hours_worked = 0
-    days_worked=0
-    
-    @classmethod
-    def check_attendance(cls):
-        attendance = random.choice(["present", "absent","half_day"])
-        return attendance
+    def __init__(self,full_day,wage_per_hour,max_work_days,max_work_hours, emp_type):
+        self.full_day = full_day
+        self.wage_per_hour = wage_per_hour
+        self.total_wage = 0
+        self.part_time_hr = 4
+        self.max_work_days= max_work_days
+        self.max_work_hours = max_work_hours
+        self.hours_worked = 0
+        self.days_worked=0
+        self.emp_type=emp_type
         
-    @classmethod
-    def calculate_wage(cls):
-        emp_status = cls.check_attendance()
-        match emp_status:
+    
+    
+    def check_attendance(self):
+        if self.emp_type!="half_day":
+            attendance = random.choice(["present", "absent"])
+            return attendance
+        else: 
+            attendance = random.choice(["absent","half_day"])
+            return attendance
+    
+    def calculate_wage(self):
+        emp_status = self.check_attendance()
+        if emp_status== 'present':
+                self.total_wage += self.wage_per_hour * self.full_day
+                self.hours_worked += self.full_day
+                # self.total_month_wage=self.wage_per_hour*self.full_day*self.total_work_days
+                print("the Employee is present on full day working")
+        if emp_status== "half_day":
+            self.total_wage += self.wage_per_hour * self.part_time_hr
+            self.hours_worked += self.part_time_hr
+            print("the Employee is on Half day working")
             
-            case 'present':
-                    cls.total_wage += cls.wage_per_hour * cls.full_day
-                    cls.hours_worked += cls.full_day
-                    # cls.total_month_wage=cls.wage_per_hour*cls.full_day*cls.total_work_days
-                    print("the Employee is present on full day working")
-            case "half_day":
-                cls.total_wage += cls.wage_per_hour * cls.part_time_hr
-                cls.hours_worked += cls.part_time_hr
-                print("the Employee is on Half day working")
-                
-            case _:
-                print("The Employee is absent")
-    @classmethod       
-    def monthly_wage(cls):
-        while cls.hours_worked < cls.max_work_hours and cls.days_worked < cls.max_work_days:
-            cls.days_worked += 1
-            cls.calculate_wage()
+        else:
+            print("The Employee is absent")
+      
+    def monthly_wage(self):
+        while int(self.hours_worked) < int(self.max_work_hours) and int(self.days_worked) < int(self.max_work_days):
+            self.days_worked += 1
+            self.calculate_wage()
 
 
-emp1= EmployeeWage()
+f_d_w=int(input("Enter 8 the full day work hours "))
+w_p_h= int(input("Enter 20 the wage per hour for your company"))
+max_w_h= int(input("Enter 100 the max working hours for your company"))
+max_w_d= int(input("Enter 25 the max working days for your company"))
+
+status= input("Enter 'half_day' a part time employee else enter full day")
+
+emp1= EmployeeWage(f_d_w,w_p_h,max_w_d,max_w_h,status)
 emp1.monthly_wage()
 print(emp1.total_wage, emp1.hours_worked, emp1.days_worked)
