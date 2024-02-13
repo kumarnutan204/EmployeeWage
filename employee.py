9f16cb83c3a7710c42195fe27e26af6b19c640fe
kumarnutan204 <kumarnutan204@gmail.com>
2024-02-13 11:14:22 +0530

    [Nutan] Added:Refactored the code to class methods and class variables

--- employee.py
+++ employee.py
@@ -1,42 +1,43 @@
 import random 
 
 class EmployeeWage:
-    def __init__(self):
-        self.wage_per_hour = 20
-        self.full_day = 8
-        self.total_wage = 0
-        self.part_time_hr = 4
-        self.max_work_days=25
-        self.max_work_hours =100
-        self.hours_worked = 0
-        self.days_worked=0
+    # def __init__(cls):
+    full_day = 8
+    wage_per_hour = 20
+    total_wage = 0
+    part_time_hr = 4
+    max_work_days=25
+    max_work_hours =100
+    hours_worked = 0
+    days_worked=0
     
-    def check_attendance(self):
+    @classmethod
+    def check_attendance(cls):
         attendance = random.choice(["present", "absent","half_day"])
         return attendance
         
-        
-    def calculate_wage(self):
-        emp_status = self.check_attendance()
+    @classmethod
+    def calculate_wage(cls):
+        emp_status = cls.check_attendance()
         match emp_status:
             
             case 'present':
-                    self.total_wage += self.wage_per_hour * self.full_day
-                    self.hours_worked += self.full_day
-                    # self.total_month_wage=self.wage_per_hour*self.full_day*self.total_work_days
+                    cls.total_wage += cls.wage_per_hour * cls.full_day
+                    cls.hours_worked += cls.full_day
+                    # cls.total_month_wage=cls.wage_per_hour*cls.full_day*cls.total_work_days
                     print("the Employee is present on full day working")
             case "half_day":
-                self.total_wage += self.wage_per_hour * self.part_time_hr
-                self.hours_worked += self.part_time_hr
+                cls.total_wage += cls.wage_per_hour * cls.part_time_hr
+                cls.hours_worked += cls.part_time_hr
                 print("the Employee is on Half day working")
                 
             case _:
                 print("The Employee is absent")
-                
-    def monthly_wage(self):
-        while self.hours_worked < self.max_work_hours and self.days_worked < self.max_work_days:
-            self.days_worked += 1
-            self.calculate_wage()
+    @classmethod       
+    def monthly_wage(cls):
+        while cls.hours_worked < cls.max_work_hours and cls.days_worked < cls.max_work_days:
+            cls.days_worked += 1
+            cls.calculate_wage()
 
 
 emp1= EmployeeWage()
